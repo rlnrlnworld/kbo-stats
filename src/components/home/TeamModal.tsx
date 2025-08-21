57ac6aa4cd529e956843014667b9a0faf943ab3a
rlnrlnworld <diiidhe@gmail.com>
2025-08-21 16:30:52 +0900

    fix: 팀 모달

--- src/components/home/TeamModal.tsx
+++ src/components/home/TeamModal.tsx
@@ -56,12 +56,12 @@ export default function TeamModal({ teamId, onClose }: Props) {
           </div>
 
           <div className="absolute left-1/2 bottom-0 transform -translate-x-1/2 translate-y-1/2">
-            <div className="w-24 h-24 bg-white rounded-full shadow-lg flex items-center justify-center border-4 border-white">
+            <div className="w-30 h-30 bg-white rounded-full shadow-lg flex items-center justify-center border-4 border-white">
               <Image
                 src={`/team-logos/${teamId}.svg`}
                 alt={`${teamName} 로고`}
-                width={64}
-                height={64}
+                width={80}
+                height={80}
                 priority
                 className="rounded-full"
               />
@@ -69,7 +69,7 @@ export default function TeamModal({ teamId, onClose }: Props) {
           </div>
         </div>
         
-        <div className="px-8 pt-16 pb-8">
+        <div className="px-8 pt-24 pb-8">
           {isLoading ? (
             <div className="text-center py-8">
               <div className="animate-pulse">
